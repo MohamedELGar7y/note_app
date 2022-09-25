7befd0bcf95e2a869fecbf0e45490737cf58a8e2
Mohamed Elgarhy <41006242+MohamedELGar7y@users.noreply.github.com>
2022-09-25 22:12:03 +0200

    Initial commit

---
+++ lib/archived_tasks/arcived_tasks_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../components/components.dart';
+import '../cubit/cubit.dart';
+import '../cubit/states.dart';
+
+class ArchivedTasksScreen extends StatelessWidget {
+  const ArchivedTasksScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<AppCubit,AppStates>(
+      listener: (context,state){},
+      builder: (context,state)
+      {
+        var tasks = AppCubit.get(context).archiveTasks;
+        return tasksBuilder(
+            tasks: tasks,
+        );
+      },
+    );
+  }
+}
